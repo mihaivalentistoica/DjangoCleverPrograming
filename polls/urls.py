6a76e6aa8db18702fc9734f7c3c2f62aa8b0beda
Stoica Mihai <mihaivalentinstoica@gmail.com>
2020-09-05 15:49:33 +0300

    Testing

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, details, results, vote
+from . import views
 
 app_name ='polls'
 urlpatterns=[
-    path('', index, name="index"),
-    path('<int:question_id>/', details, name="details"),
-    path('<int:question_id>/results', results, name="results"),
-    path('<int:question_id>/vote', vote, name="vote")
+    path('', views.IndexView.as_view(), name="index"),
+    path('<int:pk>/', views.DetailView.as_view(), name="details"),
+    path('<int:pk>/results', views.ResultsView.as_view(), name="results"),
+    path('<int:question_id>/vote', views.vote, name="vote")
 ]
